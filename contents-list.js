205105c34a4c75f5c177ff4f06dc6bd966690835
kitae.kim <kkt@i-screammedia.com>
2023-08-11 16:23:11 +0900

    workbox 테스트용 contents-list.js 추가

---
+++ contents-list.js
@@ -0,0 +1,12 @@
+const contentsList = [
+    {url: './index.html', revision: '2308111101'},
+    {url: './style.css', revision: '2308111101'},
+    {url: './image-list.js', revision: '2308111101'},
+    {url: './star-wars-logo.jpg', revision: '2308111112'},
+    {url: './gallery/myLittleVader.jpg', revision: '2308111101'},
+    {url: './gallery/snowTroopers.jpg', revision: '2308111112'},
+    {url: './gallery/bountyHunters.jpg', revision: '23081111398'},
+    {url: './app.js', revision: '2308111103'},
+];
+
+const isCache = true;

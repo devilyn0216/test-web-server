724ea55ee2eb99d6b5e33b871764c528f6c75764
kitae.kim <kkt@i-screammedia.com>
2023-08-11 17:08:58 +0900

    workbox 테스트용 contents-list.js 추가

--- contents-list.js
+++ contents-list.js
@@ -3,8 +3,8 @@ const contentsList = [
     {url: './style.css', revision: '1'},
     {url: './image-list.js', revision: '1'},
     {url: './star-wars-logo.jpg', revision: '1'},
-    {url: './gallery/myLittleVader.jpg', revision: '4'},
-    {url: './gallery/snowTroopers.jpg', revision: '4'},
+    {url: './gallery/myLittleVader.jpg', revision: '5'},
+    {url: './gallery/snowTroopers.jpg', revision: '5'},
     {url: './gallery/bountyHunters.jpg', revision: '1'},
     {url: './app.js', revision: '1'},
 ];
